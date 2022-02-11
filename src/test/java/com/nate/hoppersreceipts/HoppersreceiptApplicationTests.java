2493fcdfc0af3732d7c260c8557821a2a8db127c
Nate Trichanh <natetrichanh@gmail.com>
2022-02-10 21:10:21 -0800

    initial commit

---
+++ src/test/java/com/nate/hoppersreceipts/HoppersreceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nate.hoppersreceipts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersreceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
